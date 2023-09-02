d13cc5541662a38bc03c62a59e1082ca7363199a
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-01 23:13:28 -0300

    Se creó seeders rubros

---
+++ database/seeders/20230902020854-rubros.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const rubros = [
+      {
+        id: 1,
+        nombre_rubro: 'desarrollo',
+        createdAt: '2022-06-05 12:00:00+00',
+        updatedAt: '2022-06-05 12:00:00+00',
+      },
+      {
+        id: 2,
+        nombre_rubro: 'automotriz',
+        createdAt: '2022-06-05 12:00:00+00',
+        updatedAt: '2022-06-05 12:00:00+00',
+      },
+      {
+        id: 3,
+        nombre_rubro: 'farmaceutica',
+        createdAt: '2022-06-05 12:00:00+00',
+        updatedAt: '2022-06-05 12:00:00+00',
+      },
+      {
+        id: 4,
+        nombre_rubro: 'telecomunicaciones',
+        createdAt: '2022-06-05 12:00:00+00',
+        updatedAt: '2022-06-05 12:00:00+00',
+      },
+    ];
+
+    await queryInterface.bulkInsert('rubros', rubros, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('rubros', null, {});
+  },
+};
