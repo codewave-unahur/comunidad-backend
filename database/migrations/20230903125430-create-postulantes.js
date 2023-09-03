6444f8fa1f708d7e50164a99205e5edb5e6659bf
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-03 10:11:07 -0300

    Migraciones postulantes y relacion

---
+++ database/migrations/20230903125430-create-postulantes.js
@@ -0,0 +1,90 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('postulantes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fk_id_tipo_documento: {
+        type: Sequelize.INTEGER,
+      },
+     
+      fk_id_estudios: {
+        type: Sequelize.INTEGER,
+      },
+      fk_id_carrera: {
+        type: Sequelize.INTEGER,
+      },
+      fk_id_estado: {
+        type: Sequelize.INTEGER,
+      },
+      nombre: {
+        type: Sequelize.STRING,
+      },
+      apellido: {
+        type: Sequelize.STRING,
+      },
+      nacionalidad: {
+        type: Sequelize.STRING,
+      },
+      fecha_nac: {
+        type: Sequelize.DATE,
+      },
+      pais: {
+        type: Sequelize.STRING,
+      },
+      fk_id_provincia: {
+        type: Sequelize.INTEGER,
+      },
+      fk_id_ciudad: {
+        type: Sequelize.INTEGER,
+      },
+      calle: {
+        type: Sequelize.STRING,
+      },
+      nro: {
+        type: Sequelize.INTEGER,
+      },
+      piso: {
+        type: Sequelize.INTEGER,
+      },
+      depto: {
+        type: Sequelize.STRING,
+      },
+      cp: {
+        type: Sequelize.STRING,
+      },
+      telefono: {
+        type: Sequelize.INTEGER,
+      },
+      cant_materias: {
+        type: Sequelize.STRING,
+      },
+      alumno_unahur: {
+        type: Sequelize.BOOLEAN,
+      },
+      presentacion: {
+        type: Sequelize.STRING,
+      },
+      cv: {
+        type: Sequelize.STRING,
+      },
+      foto: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('postulantes');
+  },
+};
