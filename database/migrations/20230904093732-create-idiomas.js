b82311304857e0910ce2600d2ad0aa48ce9cab54
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-04 06:38:41 -0300

    Migracion idiomas

---
+++ database/migrations/20230904093732-create-idiomas.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('idiomas', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nombre_idioma: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('idiomas');
+  },
+};
