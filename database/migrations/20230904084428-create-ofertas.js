0dacf4cb75b95edcac08eee0290070afe6c786d5
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-04 05:54:10 -0300

    Migracion oferta y relacion empresa

---
+++ database/migrations/20230904084428-create-ofertas.js
@@ -0,0 +1,76 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ofertas', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fk_id_jornada: {
+        type: Sequelize.INTEGER
+      },
+      fk_id_contrato: {
+        type: Sequelize.INTEGER
+      },
+      fk_id_estudio: {
+        type: Sequelize.INTEGER
+      },
+      fk_id_carrera: {
+        type: Sequelize.INTEGER
+      },
+      fk_id_estado: {
+        type: Sequelize.INTEGER
+      },
+      fecha_vigencia: {
+        type: Sequelize.DATE
+      },
+      titulo_oferta: {
+        type: Sequelize.STRING
+      },
+      descripcion: {
+        type: Sequelize.TEXT
+      },
+      horario_laboral_desde: {
+        type: Sequelize.STRING
+      },
+      horario_laboral_hasta: {
+        type: Sequelize.STRING
+      },
+      edad_desde: {
+        type: Sequelize.INTEGER
+      },
+      edad_hasta: {
+        type: Sequelize.INTEGER
+      },
+      experiencia_previa_desc: {
+        type: Sequelize.STRING
+      },
+      zona_trabajo: {
+        type: Sequelize.STRING
+      },
+      areas_estudio: {
+        type: Sequelize.STRING
+      },
+      otros_detalles: {
+        type: Sequelize.STRING
+      },
+      beneficios: {
+        type: Sequelize.STRING
+      },
+      remuneracion: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+      
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ofertas');
+  },
+};
