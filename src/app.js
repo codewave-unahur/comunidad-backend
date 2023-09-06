67e30cfad8a9dd3ba09453071fa9354a1df01316
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-06 06:11:06 -0300

    cambios package.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import index_routes from './routes/index.js';
+import routes from './routes';
 import cors from "cors";
 const { sequelize } = require('../database/models');
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(cors());
-app.use('/', index_routes);
+app.use('/', routes);
 
 app.listen(app.get('port'), () => {
   console.log('Servidor conectado al puerto', app.get('port')),
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 import usuarios from './usuarios';
 import postulantes from './postulantes'
 import ofertas from './ofertas'
