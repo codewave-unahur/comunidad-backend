b34c116b9a4ac535bd274dbbe1bb7d9cc3b89829
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-09-10 03:12:51 -0300

    cambios

--- src/controllers/postulaciones.js
+++ src/controllers/postulaciones.js
@@ -75,7 +75,7 @@ export const getPorIdOferta = async (req, res) => {
         {
           as: "Postulante",
           model: models.postulantes,
-          attributes: ["id", "nombre", "apellido","fk_id_usuario","telefono","cv","foto"],
+          attributes: ["id", "nombre", "apellido","fk_id_usuario","telefono","foto","cv"],
         },
         {
           as: "Oferta",
