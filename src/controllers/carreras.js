d228b0544bb514f1bb23671c691b955b1c09a7e1
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-08 09:59:18 -0300

    Cambios en Carreras

--- src/controllers/carreras.js
+++ src/controllers/carreras.js
@@ -1,7 +1,6 @@
 const models = require("../../database/models");
 
 export const getAll = async (req, res) => {
-
   models.carreras
     .findAll({
         attributes: ["id","nombre_carrera"]
--- src/routes/carreras.js
+++ src/routes/carreras.js
@@ -3,13 +3,9 @@ import {
   getAll
 } from '../controllers/carreras';
 import { withErrorHandling } from './utils';
-import { validateToken } from '../middlewares/validador';
 
 const router = express.Router();
 
-// Aca el admin tendria que poder modificar/eliminar/crear(CRUD) las carreras.....
-// Estarian faltando funcionalidades.
-// Quizas lo de validar token quedaria bien en put y en delete.
 router.get('/', withErrorHandling(getAll));
 
 export default router;
