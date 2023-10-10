22048459800ae1e15840e92a3b88313d396cb8f2
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-10 10:20:02 -0300

    CRUD Carreras

--- database/migrations/20230903133303-create-carreras.js
+++ database/migrations/20230903133303-create-carreras.js
@@ -4,6 +4,7 @@ module.exports = {
     await queryInterface.createTable('carreras', {
       id: {
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
--- src/routes/carreras.js
+++ src/routes/carreras.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import {
-  getAll
+  createCarrera,
+  getAll,
+  getCarreraById,
+  updateCarreraById,
+  deleteCarreraById
 } from '../controllers/carreras';
 import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
-router.get('/', withErrorHandling(getAll));
+router.post('/', withErrorHandling(createCarrera));
+router.get('/', withErrorHandling(getAll))
+      .get('/:id', withErrorHandling(getCarreraById));
+router.put('/:id', withErrorHandling(updateCarreraById));
+router.delete('/:id', withErrorHandling(deleteCarreraById));
 
 export default router;
