ff3a464623cb27bfff930dcf46f9d7ee421bb662
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-10 10:34:53 -0300

    CRUD Ciudades

--- src/routes/ciudades.js
+++ src/routes/ciudades.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import {
-  getConFiltros
+  createCiudad,
+  getAll,
+  getCiudadById,
+  updateCiudadById,
+  deleteCiudadById
 } from '../controllers/ciudades';
 import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
-router.get('/', withErrorHandling(getConFiltros));
+router.post('/', withErrorHandling(createCiudad));
+router.get('/', withErrorHandling(getAll))
+      .get('/:id', withErrorHandling(getCiudadById));
+router.put('/:id', withErrorHandling(updateCiudadById));
+router.delete('/:id', withErrorHandling(deleteCiudadById));
+
 
 export default router;
