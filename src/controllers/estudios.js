8fa7338850e2123cab999d947c4560c83f9ace60
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-10 11:34:58 -0300

    CRUD Estudios

--- src/controllers/estudios.js
+++ src/controllers/estudios.js
@@ -1,11 +1,91 @@
 const models = require("../../database/models");
 
+// Obtener todos los estudios
 export const getAll = async (req, res) => {
-  models.estudios
-    .findAll({
-        attributes: ["id","nombre_estudio_estado"]
-    }).then(estudios => res.send({
-      estudios
-      
-    })).catch(() => res.sendStatus(500));
+  try {
+    const estudios = await models.estudios.findAll({
+      attributes: ["id", "nombre_estudio_estado"]
+    });
+    res.status(200).send({ estudios });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Obtener un estudio por ID
+export const getEstudioById = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const estudio = await models.estudios.findByPk(id, {
+      attributes: ["id", "nombre_estudio_estado"]
+    });
+
+    if (!estudio) {
+      return res.status(404).send('NOT FOUND');
+    }
+
+    res.status(200).send({ estudio });
+  } catch (error) {
+    res.sendStatus(500).send(error);
+  }
+};
+
+// Crear un nuevo estudio
+export const createEstudio = async (req, res) => {
+  const nombre= req.body.nombre;
+
+  try {
+    const nuevoEstudio = await models.estudios.create({
+      nombre_estudio_estado: nombre
+    });
+
+    res.status(201).send({ estudio: nuevoEstudio });
+  } catch (error) {
+    res.sendStatus(500).send(error);
+  }
+};
+
+// Actualizar un estudio existente
+export const updateEstudio = async (req, res) => {
+  const id = req.params.id;
+  const nombre = req.body.nombre;
+
+  try {
+    const estudio = await models.estudios.findByPk(id);
+
+    if (!estudio) {
+      return res.status(404).send('NOT FOUND');
+    }
+
+    await estudio.update({
+      nombre_estudio_estado: nombre
+    });
+
+    res.status(200).send({ estudio });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Eliminar un estudio
+export const deleteEstudio = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const estudio = await models.estudios.findByPk(id);
+
+    if (!estudio) {
+      return res.status(404).send('NOT FOUND');
+    }
+
+    await estudio.destroy();
+
+    res.status(200).send("OK");
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 };
--- src/routes/estudios.js
+++ src/routes/estudios.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import {
-  getAll
+  createEstudio,
+  getAll,
+  getEstudioById,
+  updateEstudio,
+  deleteEstudio
 } from '../controllers/estudios';
 import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
-router.get('/', withErrorHandling(getAll));
-
+router.post('/', withErrorHandling(createEstudio));
+router.get('/', withErrorHandling(getAll))
+      .get('/:id', withErrorHandling(getEstudioById));
+router.put('/:id', withErrorHandling(updateEstudio));
+router.delete('/:id', withErrorHandling(deleteEstudio));
 export default router;
