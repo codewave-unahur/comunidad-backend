fead46e21c7e6fc95c333bde755cd1644ece7775
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-11 00:07:52 -0300

    CRUD Niveles de Idiomas

--- src/controllers/nivelesIdiomas.js
+++ src/controllers/nivelesIdiomas.js
@@ -1,29 +1,90 @@
 const models = require("../../database/models");
 
+// Obtener todos los niveles de idiomas
 export const getAll = async (req, res) => {
-
-  models.niveles_idiomas
-    .findAll({
-        attributes: ["id","nivel"]
-    }).then(nivele_idioma => res.send({
-      nivele_idioma
-      
-    })).catch(() => res.sendStatus(500));
+  try {
+    const niveles = await models.niveles_idiomas.findAll({
+      attributes: ["id", "nivel"]
+    });
+    res.status(200).json({ niveles });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 };
 
-export const getById = async (req, res) => {
-  const { id } = req.params;
+// Obtener un nivel de idioma por su ID
+export const getNivelIdiomaById = async (req, res) => {
+  const nivelId = req.params.id;
 
   try {
-    const nivelIdioma = await models.niveles_idiomas.findOne({
-      where: { id },
-      attributes: ["id", "nivel"],
+    const nivel = await models.niveles_idiomas.findOne({
+      where: { id: nivelId },
+      attributes: ["id", "nivel"]
     });
 
-    if (nivelIdioma) {
-      res.send({ nivelIdioma });
+    if (nivel) {
+      res.status(200).json({ nivel });
     } else {
-      res.status(404).send("Not Found");
+      res.status(404).json("NOT FOUND");
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Crear un nuevo nivel de idioma
+export const createNivelIdioma = async (req, res) => {
+  const nivel = req.body.nivel;
+
+  try {
+    const newNivelIdioma = await models.niveles_idiomas.create({
+      nivel: nivel
+    });
+
+    res.status(200).json({ nuevoNivel: newNivelIdioma });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Actualizar un nivel de idioma existente
+export const updateNivelIdioma = async (req, res) => {
+  const nivelId = req.params.id;
+  const { nivel } = req.body;
+
+  try {
+    const nivelIdioma = await models.niveles_idiomas.findByPk(nivelId);
+
+    if (!nivelIdioma) {
+      res.status(404).send("NOT FOUND");
+    } else {
+      await nivelIdioma.update({
+        nivel
+      });
+
+      res.status(200).json({ nivelIdioma });
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Eliminar un nivel de idioma por su ID
+export const deleteNivelIdioma = async (req, res) => {
+  const nivelId = req.params.id;
+
+  try {
+    const nivelIdioma = await models.niveles_idiomas.findByPk(nivelId);
+
+    if (!nivelIdioma) {
+      res.status(404).send("NOT FOUND");
+    } else {
+      await nivelIdioma.destroy();
+      res.status(200).send("OK");
     }
   } catch (error) {
     console.error(error);
--- src/routes/nivelesIdiomas.js
+++ src/routes/nivelesIdiomas.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import {
+  createNivelIdioma,
   getAll,
-  getById
+  getNivelIdiomaById,
+  updateNivelIdioma,
+  deleteNivelIdioma
 } from '../controllers/nivelesIdiomas';
 import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
-router.get('/', withErrorHandling(getAll));
-router.get("/niveles/:id", withErrorHandling(getById));
+router.post('/', withErrorHandling(createNivelIdioma));
+router.get('/', withErrorHandling(getAll))
+      .get("/:id", withErrorHandling(getNivelIdiomaById));
+router.put('/:id', withErrorHandling(updateNivelIdioma));
+router.delete('/:id', withErrorHandling(deleteNivelIdioma));
+
 export default router;
