533a4c03b667f061aa12c150f1f499d3c13281c0
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-11 00:57:01 -0300

    Ciudad por id

--- src/routes/provincias.js
+++ src/routes/provincias.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {
-  getConFiltros
+  getConFiltros,
+  getProvinciaById
 } from '../controllers/provincias';
 import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
-router.get('/', withErrorHandling(getConFiltros));
+router.get('/', withErrorHandling(getConFiltros))
+      .get('/:id', withErrorHandling(getProvinciaById));
 
 export default router;
