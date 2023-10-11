241b0de00a01bec34021a19485784eefc203d800
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-11 16:05:02 -0300

    Cambios en utils

--- database/utils/utils.js
+++ database/utils/utils.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const filePath = path.resolve(__dirname, '../../../comunidad-backend-v3/database/models/tipos_documentos.json');
 
-export const saveToDatabase = (DB) => {
+const saveToDatabase = (DB) => {
     fs.writeFileSync(filePath, JSON.stringify(DB, null, 2), {
         encoding: "utf-8",
     });
@@ -12,9 +12,10 @@ export const saveToDatabase = (DB) => {
 // Esto habria que refactoriazar pasar la ruta por parametro a saveToDatabase y listo.
 const filePath2 = path.resolve(__dirname, '../../../comunidad-backend-v3/database/models/estados.json');
 
-export const saveToDatabase2 = (DB) => {
+const saveToDatabase2 = (DB) => {
     fs.writeFileSync(filePath2, JSON.stringify(DB, null, 2), {
         encoding: "utf-8",
     });
 };
 
+module.exports = { saveToDatabase, saveToDatabase2 };
