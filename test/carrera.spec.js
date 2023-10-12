cc752d30bf8caeff83f692fd83bbb3f7655f5aeb
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-12 09:13:11 -0300

    Cambios en carreras y packege.json

--- test/carrera.spec.js
+++ test/carrera.spec.js
@@ -3,15 +3,29 @@ import request from 'supertest';
 const api = request('http://localhost:3001');
 
 // Primero que nada existe un tema con los test, dependiendo de la cantidad de datos y id que contenga la tabla carreras
-// varia el id a la hora de crearlo, es decir, sin tengo 15 carreras, se agrega una con el id 16. 
+// varia el id a la hora de crearlo, es decir, sin tengo 15 carreras, se agrega una mas con el id 16. 
 // Porque esta programado para que el id de la carrera sea incremental. Aca todo bien...
 // El problema esta a la hora de hacer post porque tirar error 15 veces porque existe los ids en el id 16 te agrega la carrera 
-// jajaj shor o😟
+// jajaj shoro😟
 
-// Este test funciona si la base tiene un seed....
+// Demigra y seeedea y anda.
+// Este test funciona si la base tiene un seed.... 
 describe('Carrera', () => {
   let id = 1;
 
+  test('GET /carrera', async () => {
+    const response = await api.get('/carreras/' + id);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        carrera: expect.objectContaining({
+          id: expect.any(Number),
+          nombre_carrera: expect.any(String),
+        }),
+      })
+    );
+  });
+  
   test('DELETE /carrera/:id', async () => {
     const response = await api.delete('/carreras/' + id);
     expect(response.status).toBe(200);
@@ -37,6 +51,7 @@ describe('Carrera', () => {
       })
     );
   });
+
   test('POST /carrera', async () => {
     const response = await api.post('/carreras').send({
       nombre_carrera: 'Tecnicatura en informaticaaaaaaa',
