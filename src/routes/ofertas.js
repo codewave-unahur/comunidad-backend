a6b04f5b22b319872bd9020fa301e19e98c82c9e
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-10-18 08:28:39 -0300

    Cambios

--- src/routes/ofertas.js
+++ src/routes/ofertas.js
@@ -12,12 +12,25 @@ import { withErrorHandling } from './utils';
 
 const router = express.Router();
 
+// Obtener todas las ofertas
+router.get('/all/', withErrorHandling(getOfertas));
+
+// Obtener una oferta por ID
+router.get('/:id', withErrorHandling(getIdOferta));
+
+// Obtener ofertas filtradas
+router.get('/', withErrorHandling(getOfertasPorFiltros));
+
+// Obtener ofertas por ID de empresa
+router.get('/cuit/:id', withErrorHandling(getOfertasPorIdEmpresa));
+
+// Crear una nueva oferta
 router.post('/', withErrorHandling(createOferta));
-router.get('/all/', withErrorHandling(getOfertas))
-      .get('/', withErrorHandling(getOfertasPorFiltros))
-      .get('/idOferta/:id', withErrorHandling(getIdOferta))
-      .get('/cuit/:id', withErrorHandling(getOfertasPorIdEmpresa));
+
+// Actualizar una oferta por ID
 router.put('/idOferta/:id', withErrorHandling(updateOfertas));
+
+// Eliminar una oferta por ID
 router.delete('/idOferta/:id', withErrorHandling(deleteOferta));
 
 export default router;
